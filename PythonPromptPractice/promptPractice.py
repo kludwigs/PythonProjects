e70a21d6f2741f3a5a54b469110c5634f29ff959
kludwigs <kludwigs@gmail.com>
2015-10-01 19:17:18 -0700

    Prompting user
    using input and if else

---
+++ PythonPromptPractice/promptPractice.py
@@ -0,0 +1,12 @@
+print("You've just entered the clinic!")
+print("Do you take the door on the left or the right?")
+answer = input("Type left or right and hit 'Enter'.\n").lower()
+print("you entered--\t" + answer + "\n");
+if answer =="left":
+    print("Sorry, you chose poorly. You lose")
+if answer=="right":
+    print("You choose wisely. You win")
+else:
+    print("I don't understand what you're saying")
+
+    
