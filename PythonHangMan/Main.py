b355b97036c5daaabf9da7d9137921e47d33c145
kludwigs <kludwigs@gmail.com>
2015-10-28 23:25:05 -0700

    Multiple Improvements
    increases guesses, fixed bug where you can win hangman with just single
    letter guesses. Made outputs more readable

--- PythonHangMan/Main.py
+++ PythonHangMan/Main.py
@@ -8,7 +8,7 @@ import sys
 print("Welcome to Karl's hang man game")
 print("Would you like to play?")
 
-answer = input("Type y or n and hit 'Enter'.\n").lower()
+answer = raw_input("Type y or n and hit 'Enter'.\n").lower()
 
 action = startprompt.hangmanstartprompt(answer)
 
@@ -24,18 +24,18 @@ thewordcopy = "-" * len( theword )
 number_of_characters = len(theword)
 print("Okay the word has ", number_of_characters, " characters")
 
-number_of_guesses = 5
+number_of_guesses = 8
 
 print("Enter a letter to begin guessing. You'll start with ", number_of_guesses, " guesses")
-print(theword)
+#print(theword)
 #show the number of characters in the word
 
 correct_letter_positions = []
-
-while number_of_guesses > 0:
+win = False
+while number_of_guesses > 0 and win == False:
     
     print(thewordcopy)
-    guess = input("Go!\n").lower()
+    guess = raw_input("Go!\n").lower()
     if not validateguess.isacharacter(guess):
         print("That's not even a character! Try again")
     else:
@@ -43,17 +43,25 @@ while number_of_guesses > 0:
         if(len(guess) > 1):           
            if not validateguess.checkword(guess, theword):               
                print("Oops, that's not right")
+               number_of_guesses -=1
            else:
+               win = true
                print("You are correct!\nThe word is " + guess + "\nYou win!")
                break
         else:               
            correct_letter_positions = validateguess.checkcharacter(guess,theword)
            if correct_letter_positions:
                for i in correct_letter_positions:                   
-                   thewordcopy = thewordcopy[:i] + guess + thewordcopy[i+1:]   
-        number_of_guesses -=1
-    print("You have ", number_of_guesses, " remaining...")
-    
+                   thewordcopy = thewordcopy[:i] + guess + thewordcopy[i+1:]
+               if thewordcopy == theword:
+                  win = True
+                  print("You Win\nWord is " + thewordcopy + "!\n")
+                  break
+           else:
+              number_of_guesses -=1
+    print("You have " , number_of_guesses , " remaining...")
+if (win == False):
+   print("Word was " + theword + ".")
 print("Game over...")                
 
 
